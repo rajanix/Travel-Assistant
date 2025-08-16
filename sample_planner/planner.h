dcbdd6c9f2e7417d6086f0cefde8b2c0f23762b6
Abhishek Raj Soni <132194557+rajanix@users.noreply.github.com>
2025-08-17 02:37:43 +0530

    Add files via upload

---
+++ sample_planner/planner.h
@@ -0,0 +1,20 @@
+#ifndef PLANNER_H
+#define PLANNER_H
+
+struct Station {
+    // define suitable fields here
+};
+
+struct Journey {
+    // define suitable fields here
+};
+
+struct Query{
+    // define suitable fields here
+};
+
+class Planner {
+    // define suitable fields here
+};
+
+#endif
